58d1a26d8981e0d6b3e9ef879dc24f746fdc3e27
xlb <913971360@qq.com>
2018-11-13 09:43:42 +0800

    first init

---
+++ src/main/java/com/jks/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.jks.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @类名：A
+ * @描述：
+ * @创建人 xlb
+ * @创建时间 2018/11/12 16:18
+ * @Version 1.0
+ */
+    @RestController
+    public class HelloController {
+        @RequestMapping("/hello")
+        public String hello() {
+            return "Hello Spring Boot!";
+        }
+    }
---
+++ src/test/java/com/jks/jks/JksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jks.jks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
